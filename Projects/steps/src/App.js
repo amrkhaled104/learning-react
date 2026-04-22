15f38bb96a7b0dff5076aae0fb8df7bc7bfbdc59
amr <amrkhaledelsisy@gmail.com>
2026-04-22 15:45:29 +0200

    reusable button in step project

--- Projects/steps/src/App.js
+++ Projects/steps/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <div className="steps" style={isopen ? {} : { display: "none" }}>
         <Steps stepsnumber={3} />
       </div>
-      <div className="close" onClick={() => setisopen(prev => !prev)}>
+      <div className="close" onClick={() => setisopen((prev) => !prev)}>
         x
       </div>
     </>
@@ -47,24 +47,34 @@ function Steps({ stepsnumber }) {
         {" "}
         Step {step}:{Messages[step - 1]}
       </p>
-
       <div className="buttons">
-        <button
+        <Button
+          bgColor={step > 1 ? "#7950f2" : "#e7e7e7"}
+          textColor={step > 1 ? "#fff" : "#333"}
           onClick={handlePrevious}
-          style={step > 1 ? { backgroundColor: "#7950f2", color: "white" } : {}}
         >
-          <span>Previous</span>
-        </button>
-        <button
+          <span>👈</span> Previous
+        </Button>
+
+        <Button
+          bgColor={step < length ? "#7950f2" : "#e7e7e7"}
+          textColor={step < length ? "#fff" : "#333"}
           onClick={handleNext}
-          style={
-            step < length ? { backgroundColor: "#7950f2", color: "white" } : {}
-          }
         >
-          <span>Next</span>
-        </button>
+          Next <span>👉</span>
+        </Button>
       </div>
     </>
+  );
+}
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
